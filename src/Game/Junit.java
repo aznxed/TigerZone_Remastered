f41604e99ec43f087b989de297785b0f73be1653
Marisa Fernandez <Marisa@Marisas-MBP.att.net>
2016-11-29 17:56:09 -0500

    More tests added for isValid and getNeigbors

--- src/Game/Junit.java
+++ src/Game/Junit.java
@@ -3,6 +3,9 @@ package Game;
 import org.junit.Test;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
+
+import Game.TerrainType;
+
 import java.util.List;
 
 import static org.junit.Assert.*;
@@ -13,17 +16,28 @@ public class Junit {
 
 	// TerrainType[] for testing purposes
 	// Continuation road to right
-	static TerrainType[] tileTR = { TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE,
-			TerrainType.GAMETRAIL, TerrainType.GAMETRAIL, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-			TerrainType.JUNGLE };
+	static TerrainType[] tileTR = { TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE, 
+			TerrainType.JUNGLE,TerrainType.GAMETRAIL, TerrainType.GAMETRAIL, 
+			TerrainType.JUNGLE, TerrainType.GAMETRAIL,TerrainType.JUNGLE };
 
 	// Continuation road to left
-	static TerrainType[] tileTL = { TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL,
-			TerrainType.GAMETRAIL, TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.JUNGLE };
+	static TerrainType[] tileTL = { TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE, 
+			TerrainType.GAMETRAIL,TerrainType.GAMETRAIL, TerrainType.JUNGLE, 
+			TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.JUNGLE };
+
+	static TerrainType[] tileBL = { TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.JUNGLE, 
+			TerrainType.JUNGLE,TerrainType.GAMETRAIL, TerrainType.GAMETRAIL, 
+			TerrainType.JUNGLE, TerrainType.JUNGLE, TerrainType.JUNGLE };
+
+	static TerrainType[] tileBR = { TerrainType.JUNGLE, TerrainType.GAMETRAIL, TerrainType.JUNGLE,
+			TerrainType.GAMETRAIL, TerrainType.GAMETRAIL, TerrainType.JUNGLE, 
+			TerrainType.JUNGLE, TerrainType.JUNGLE,TerrainType.JUNGLE };
 
 	static Board testBoard = new Board();
 	static Tile testTile1 = new Tile(tileTR);
 	static Tile testTile2 = new Tile(tileTL);
+	static Tile testTile3 = new Tile(tileBL);
+	static Tile testTile4 = new Tile(tileBR);
 
 	// Test tiles are being placed on board
 	@Test
@@ -48,7 +62,7 @@ public class Junit {
 	public void testC() {
 
 		assertEquals(testTile1, testBoard.getPlacedTile().get(0));
-		//testBoard.removeTile(77, 77, testTile1);
+		// testBoard.removeTile(77, 77, testTile1);
 	}
 
 	// Test isValid
@@ -62,14 +76,24 @@ public class Junit {
 	@Test
 	public void testE() {
 
-		int result = testBoard.placeTile(77, 78, 0, testTile2);
-		assertEquals(result, 1);
+		int result1 = testBoard.placeTile(77, 78, 0, testTile2);
+		assertEquals(result1, 1);
+		int result2 = testBoard.placeTile(78, 78, 0, testTile3);
+		assertEquals(result2, 1);
+		int result3 = testBoard.placeTile(78, 77, 0, testTile4);
+		assertEquals(result3, 1);
+
+		List<Tile> nbors1 = testBoard.getNeighbors(77, 77);
+		assertEquals(nbors1.size(), 2);
+
+		List<Tile> nbors2 = testBoard.getNeighbors(77, 78);
+		assertEquals(nbors2.size(), 2);
 		
-		List<Tile> nbors1 = testBoard.getNeighbors(77, 78);
-		assertEquals(nbors1.size(), 1);
+		List<Tile> nbors3 = testBoard.getNeighbors(78, 78);
+		assertEquals(nbors3.size(), 2);
 		
-		List<Tile> nbors2 = testBoard.getNeighbors(77, 77);
-		assertEquals(nbors2.size(), 1);
+		List<Tile> nbors4 = testBoard.getNeighbors(78, 77);
+		assertEquals(nbors4.size(), 2);
 	}
 
 }
