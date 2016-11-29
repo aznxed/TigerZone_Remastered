5aae6dbe4cfe930f8567405175e33ff8a22284dc
aznxed <aznxed@ufl.edu>
2016-11-29 12:05:50 -0500

    Board and Tile edits

--- src/Game/Board.java
+++ src/Game/Board.java
@@ -14,11 +14,74 @@ public class Board {
 	private int leftBound;
 	private int rightBound;
 	
+	public Deck deck = new Deck();
+	private Tile[][] board = new Tile[MAX_ROWS][MAX_COLS];
+	private List<Tile> placedTiles = new ArrayList<Tile>();
+	
+	//Is this needed?
+	private boolean startTile = true;
+	
+	/*******************************************/
+	/******* Getter and Setter Functions *******/
+	/*******************************************/
+	
+	public int getTopBound() {
+		return this.topBound;
+	}
+
+	public int getBottomBound() {
+		return this.bottomBound;
+	}
+
+	public int getLeftBound() {
+		return this.leftBound;
+	}
+
+	public int getRightBound() {
+		return this.rightBound;
+	}
+
+	public void setTopBound(int upperBound) {
+		this.topBound = upperBound;
+	}
+
+	public void setBottomBound(int lowerBound) {
+		this.bottomBound = lowerBound;
+	}
+
+	public void setLeftBound(int leftBound) {
+		this.leftBound = leftBound;
+	}
+
+	public void setRightBound(int rightBound) {
+		this.rightBound = rightBound;
+	}
+	
+	public Tile[][] getBoard(){
+		return this.board;
+	}
+	
+	public Tile getTile(int x, int y) {
+		if (x >= 0 && x < MAX_ROWS && y >= 0 && y < MAX_COLS) {
+			return board[x][y];
+		}
+		return null;
+	}
+	
+	/********************************/
+	/******* Player Functions *******/
+	/********************************/
+	
 	//Place a Tile
 	public int placeTile(int x, int y, int rotation, Tile tile){
 		
 		return 1;
 	}
+	
+	
+	/*********************************/
+	/******* Testing Functions *******/
+	/*********************************/
 	
 	//Print the Board
 	public int printBoard(){
@@ -26,5 +89,54 @@ public class Board {
 		return 1;
 	}
 	
+	//Get Neighboring Tiles
+	//TEST NOT COMPLETE
+	public List<Tile> getNeighbors(int x, int y) {
+		List<Tile> n = new ArrayList<Tile>();
+
+		if (y > 0) {
+			if (board[x][y - 1] != null) {
+				n.add(board[x][y - 1]);
+			}
+		}
+
+		if (y < MAX_COLS) {
+			if (board[x][y + 1] != null) {
+				n.add(board[x][y + 1]);
+			}
+		}
+
+		if (x > 0) {
+			if (board[x - 1][y] != null) {
+				n.add(board[x - 1][y]);
+			}
+		}
+
+		if (x < MAX_ROWS) {
+			if (board[x + 1][y] != null) {
+				n.add(board[x + 1][y]);
+			}
+		}
+
+		return n;
+	}
+	
+	//Can I place this tile here?
+	//TEST NOT COMPLETED
+	public boolean isValid(int x, int y, Tile tile) {
+		// Space already has tile
+		if (board[x][y] != null) {
+			return false;
+		}
+
+		List<Tile> nbors = getNeighbors(x, y);
+
+		// No neighbors
+		if (nbors.isEmpty()) {
+			return false;
+		}
+
+		return true;
+	}
 	
 }
--- src/Game/Tile.java
+++ src/Game/Tile.java
@@ -1,9 +1,68 @@
 package Game;
 
+enum TerrainType {
+	GAMETRAIL, JUNGLE, LAKE, BUFFALO, CROCODILE, DEER, BOAR, DEN, END
+}
+
 public class Tile {
 	private int row;
 	private int col;
 	private String terrainTypeString;
 	private int degrees;
 	
+	private TerrainType[] tilePortionType = new TerrainType[9];
+	
+	/*******************************************/
+	/******* Getter and Setter Functions *******/
+	/*******************************************/
+	
+	public int getRow(){
+		return row;
+	}
+	
+	public int getCol(){
+		return col;
+	}
+	
+	public int getDegrees(){
+		return degrees;
+	}
+	
+	public String getTileType(){
+		return terrainTypeString;
+	}
+	
+	public TerrainType getTopEdge() {
+		return tilePortionType[1];
+	}
+
+	public TerrainType getBottomEdge() {
+		return tilePortionType[7];
+	}
+
+	public TerrainType getLeftEdge() {
+		return tilePortionType[3];
+	}
+
+	public TerrainType getRightEdge() {
+		return tilePortionType[5];
+	}
+	
+	
+	public Tile(String terrainTypeString, int row, int col, int degrees){
+		this.terrainTypeString = terrainTypeString;
+		this.row = row;
+		this.col = col;
+		this.degrees = degrees;
+		
+		//Switch to create array of TerrainType
+		
+		//Rotate if degrees != 0
+		
+		
+	}
+	
+	
+	
+
 }
