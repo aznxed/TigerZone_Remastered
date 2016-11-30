d46c3401b52c2b41ebe3a1b4ec713758e16652cb
Marisa Fernandez <Marisa@Marisas-MacBook-Pro.local>
2016-11-29 21:28:48 -0500

    Update

--- src/Game/Tile.java
+++ src/Game/Tile.java
@@ -17,6 +17,9 @@ public class Tile {
 	
 	private TerrainType[] tilePortionType = new TerrainType[9];
 	
+	//
+	private int type;
+	
 	/*******************************************/
 	/******* Getter and Setter Functions *******/
 	/*******************************************/
@@ -70,11 +73,32 @@ public class Tile {
 	public void setBoard(Board board) {
 		this.board = board;
 	}
+	
+	public TerrainType[] getTilePortionType() {
+		return tilePortionType;
+	}
+
+	public void setTilePortionType(TerrainType[] tilePortionType) {
+		this.tilePortionType = tilePortionType;
+	}
+	
+	public int getType() {
+		return type;
+	}
+	
+	
 
 	public Tile(TerrainType[] tilePortionType){
 		this.tilePortionType = tilePortionType;
 	}
+	
+	public Tile(TerrainType[] tilePortionType, int row, int col, int degrees){
+		this.tilePortionType = tilePortionType;
+	}
+	
+	
 	/*public Tile(String terrainTypeString, int row, int col, int degrees){
+		
 		this.terrainTypeString = terrainTypeString;
 		this.row = row;
 		this.col = col;
