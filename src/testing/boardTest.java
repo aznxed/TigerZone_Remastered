5c343c695bc37fd7e22590967b958de8db931502
Spencer <Spencer@10.136.20.151>
2016-12-01 19:21:37 -0500

    Cleanup boardTest

--- src/testing/boardTest.java
+++ src/testing/boardTest.java
@@ -28,29 +28,6 @@ public class boardTest {
 			gameBoard.addTile(deck.getTop());
 		}
 		
-		
-		
-		
-		/*
-		gameBoard.placeTile(CENTER_CELL, CENTER_CELL, 0, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL+1, CENTER_CELL, 270, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL, CENTER_CELL+1, 90, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL, CENTER_CELL-1, 0, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL-1, CENTER_CELL, 90, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL, CENTER_CELL-2, 180, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL+2, CENTER_CELL, 0, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL+2, CENTER_CELL-1, 270, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL+1, CENTER_CELL+1, 270, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL-1, CENTER_CELL+1, 270, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL, CENTER_CELL+2, 180, deck.getTop());
-		gameBoard.placeTile(CENTER_CELL-2, CENTER_CELL, 90, deck.getTop());
-		*/
-
-		//System.out.println(pos.size());
-		//gameBoard.addTile(deck.getTop());
-		//gameBoard.placeTile(CENTER_CELL+1, CENTER_CELL, 90, tile2);
-		//System.out.println(tile2.getCol());
-		
 		//Print out the board
 		gameBoard.print();
 	}
